0ca8bfc880cd3d1312e5fdcdb94b815fd3552888
anniesu1 <anniesumail@gmail.com>
2019-02-07 01:58:25 -0500

    implemented ray tracing

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -8,6 +8,187 @@ uniform float u_Time;
 in vec2 fs_Pos;
 out vec4 out_Col;
 
+const int MAX_MARCHING_STEPS = 255;
+const float MIN_DIST = 0.0;
+const float MAX_DIST = 100.0;
+const float EPSILON = 0.0001;
+
+/**
+ * SDF cominbation operations 
+ */ 
+float intersectSDF(float distA, float distB) {
+    return max(distA, distB);
+}
+
+float unionSDF(float distA, float distB) {
+    return min(distA, distB);
+}
+
+float differenceSDF(float distA, float distB) {
+    return max(distA, -distB);
+}
+
+/** 
+ * Signed distance functions (SDF)
+ */
+float cubeSDF(vec3 p) {
+    // If d.x < 0, then -1 < p.x < 1, and same logic applies to p.y, p.z
+    // So if all components of d are negative, then p is inside the unit cube
+    vec3 d = abs(p) - vec3(1.0, 1.0, 1.0);
+    
+    // Assuming p is inside the cube, how far is it from the surface?
+    // Result will be negative or zero.
+    float insideDistance = min(max(d.x, max(d.y, d.z)), 0.0);
+    
+    // Assuming p is outside the cube, how far is it from the surface?
+    // Result will be positive or zero.
+    float outsideDistance = length(max(d, 0.0));
+    
+    return insideDistance + outsideDistance;
+}
+
+float sphereSDF(vec3 p) {
+    return length(p) - 1.0;
+}
+
+// From Inigo Quilez
+float roundBoxSDF( in vec3 p, in vec3 b, in float r ) {
+    vec3 q = abs(p) - b;
+    return min(max(q.x,max(q.y,q.z)),0.0) + length(max(q,0.0)) - r;
+}
+
+float sdRoundCone( in vec3 p, in float r1, float r2, float h )
+{
+    vec2 q = vec2( length(p.xz), p.y );
+    
+    float b = (r1-r2)/h;
+    float a = sqrt(1.0-b*b);
+    float k = dot(q,vec2(-b,a));
+    
+    if( k < 0.0 ) return length(q) - r1;
+    if( k > a*h ) return length(q-vec2(0.0,h)) - r2;
+        
+    return dot(q, vec2(a,b) ) - r1;
+}
+
+
+float sceneSDF(vec3 samplePoint) {
+    float sphereDist = sphereSDF(samplePoint / 1.2) * 1.2;
+    float cubeDist = cubeSDF(samplePoint + vec3(0.0, sin(u_Time), 0.0));
+    //return intersectSDF(cubeDist, sphereDist);
+    //return sphereSDF(samplePoint);
+    return cubeSDF(samplePoint);
+}
+
+float shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, float end) {
+    float depth = start;
+    for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
+        float dist = sceneSDF(eye + depth * marchingDirection);
+        if (dist < EPSILON) {
+			return depth;
+        }
+        depth += dist;
+        if (depth >= end) {
+            return end;
+        }
+    }
+    return end;
+}
+
+vec3 estimateNormal(vec3 p) {
+    return normalize(vec3(
+        sceneSDF(vec3(p.x + EPSILON, p.y, p.z)) - sceneSDF(vec3(p.x - EPSILON, p.y, p.z)),
+        sceneSDF(vec3(p.x, p.y + EPSILON, p.z)) - sceneSDF(vec3(p.x, p.y - EPSILON, p.z)),
+        sceneSDF(vec3(p.x, p.y, p.z  + EPSILON)) - sceneSDF(vec3(p.x, p.y, p.z - EPSILON))
+    ));
+}
+
+vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
+                          vec3 lightPos, vec3 lightIntensity) {
+    vec3 N = estimateNormal(p);
+    vec3 L = normalize(lightPos - p);
+    vec3 V = normalize(eye - p);
+    vec3 R = normalize(reflect(-L, N));
+    
+    float dotLN = dot(L, N);
+    float dotRV = dot(R, V);
+    
+    if (dotLN < 0.0) {
+        // Light not visible from this point on the surface
+        return vec3(0.0, 0.0, 0.0);
+    } 
+    
+    if (dotRV < 0.0) {
+        // Light reflection in opposite direction as viewer, apply only diffuse
+        // component
+        return lightIntensity * (k_d * dotLN);
+    }
+    return lightIntensity * (k_d * dotLN + k_s * pow(dotRV, alpha));
+}
+
+vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye) {
+    const vec3 ambientLight = 0.5 * vec3(1.0, 1.0, 1.0);
+    vec3 color = ambientLight * k_a;
+    
+    vec3 light1Pos = vec3(4.0 * sin(u_Time),
+                          2.0,
+                          4.0 * cos(u_Time));
+    vec3 light1Intensity = vec3(0.4, 0.4, 0.4);
+    
+    color += phongContribForLight(k_d, k_s, alpha, p, eye,
+                                  light1Pos,
+                                  light1Intensity);
+    
+    vec3 light2Pos = vec3(2.0 * sin(0.37 * u_Time),
+                          2.0 * cos(0.37 * u_Time),
+                          2.0);
+    vec3 light2Intensity = vec3(0.4, 0.4, 0.4);
+    
+    color += phongContribForLight(k_d, k_s, alpha, p, eye,
+                                  light2Pos,
+                                  light2Intensity);    
+    return color;
+}
+
 void main() {
-  out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
+  float sx = fs_Pos[0];
+  float sy = fs_Pos[1];
+
+  // Form a ray
+  float len = length(u_Ref - u_Eye);
+  vec3 V = u_Up * len * tan(45.0 / 2.0); // Note: 45 is the FOV
+  vec3 R = normalize(cross(u_Up, u_Eye - u_Ref));
+  float aspect = u_Dimensions[0] / u_Dimensions[1];
+  vec3 H = R * len * aspect * tan(45.0);
+
+  // Cast a ray
+  vec3 p = u_Ref + sx * H + sy * V;
+  vec3 dir = normalize(p - u_Eye);
+
+  float dist = shortestDistanceToSurface(u_Eye, dir, MIN_DIST, MAX_DIST);
+
+  // Lambert's Law for shading
+  //vec3 normal = estimateNormal(vec3(fs_Pos, 1.0));
+  // float diffuseTerm = dot(normalize(normal), normalize(fs_LightVec));
+  // diffuseTerm = clamp(diffuseTerm, 0.0, 1.0);
+
+  if (dist > MAX_DIST - EPSILON) {
+    // Didn't hit anything
+    out_Col = vec4(0.0, 0.0, 0.0, 0.0);
+		//return;
+  }
+    
+  // The closest point on the surface to the eyepoint along the view ray
+  vec3 pClosest = u_Eye + dist * dir;
+    
+  vec3 K_a = vec3(0.2, 0.2, 0.2);
+  vec3 K_d = vec3(0.7, 0.2, 0.2);
+  vec3 K_s = vec3(1.0, 1.0, 1.0);
+  float shininess = 10.0;
+    
+  //vec3 color = phongIllumination(K_a, K_d, K_s, shininess, pClosest, u_Eye);
+
+  vec3 color = 0.5 * (dir + vec3(1.0, 1.0, 1.0));
+  out_Col = vec4(color, 1.0);
+  //out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
 }
